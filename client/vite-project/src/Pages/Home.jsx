7e6d5b4cfa6be9ab853a786efe5a5d87dfbd1794
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-15 13:32:30 +0530

    Add profile navigation from home

--- client/vite-project/src/Pages/Home.jsx
+++ client/vite-project/src/Pages/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 
 function Home() {
-  const {user} = useAuth()
+  const { user } = useAuth()
+
   return (
     <div>
-    <h1 className='text-4xl'>Home</h1>
-    <h3 className='text-4xl'>{user.username}</h3>
+      <h1 className='text-4xl'>Home</h1>
+      <h3 className='text-4xl'>{user?.username}</h3>
+      <Link
+        to='/profile'
+        className='inline-block mt-4 px-4 py-2 rounded-lg bg-slate-900 text-white'
+      >
+        Go to Profile
+      </Link>
     </div>
   )
 }
 
 export default Home
-
-
-// Public Routes - Resources accesible publicly
-// Protected Routes - verifivation required to access
